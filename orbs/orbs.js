36b975529db3fd9c8b98ed2e4dcf1e704f01d6ed
squffed <99331854+squffed@users.noreply.github.com>
2022-03-17 14:47:23 +0000

    Fixed width and height for orbs canvas

--- orbs/orbs.js
+++ orbs/orbs.js
@@ -240,14 +240,6 @@ function orbPhysics()
     setTimeout(draw, interval);
 }
 
-function checkCanvasBounds()
-{
-    if(window.innerWidth < 720)
-    {
-        ctx.canvas.width = window.innerWidth;
-    }
-}
-
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawOrbs();
